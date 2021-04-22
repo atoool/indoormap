3100d673667bbb08888d27655bf49782edc5c510
atoool <scrapycoco.sam@gmail.com>
2021-04-22 21:46:49 +0530

    multidex enabled

--- android/app/src/main/java/com/indoormap/MainApplication.java
+++ android/app/src/main/java/com/indoormap/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.viromedia.bridge.ReactViroPackage;
+import androidx.multidex.MultiDexApplication;
 
 public class MainApplication extends Application implements ReactApplication {
 
